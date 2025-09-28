481ba9de6ba0d11c12150d8bd382db695b82c1aa
Rahman Tech <rahmantech22@gmail.com>
2025-09-29 01:49:45 +0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -5,7 +5,7 @@ const { cmd } = require("../command");
 
 cmd({
   pattern: "play",
-  alias: ["song", "music"],   
+  alias: ["sons", "music"],   
   desc: "Download YouTube audio by title",
   category: "download",
   react: "🎶",
